f3f0bf491e89de7c54a995a6d31484bb230bed4a
liapkin <146948686+ntua-el13656@users.noreply.github.com>
2024-05-19 17:51:15 +0300

    Delete queries/09 directory

--- queries/09/09.indexes.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX idx_ingredient_quantities_recipe_id ON ingredient_quantities (recipe_id);
-CREATE INDEX idx_ingredient_quantities_ingredient_id ON ingredient_quantities (ingredient_id);
-CREATE INDEX idx_nutritional_info_ingredient_id ON nutritional_info (ingredient_id);
-CREATE INDEX idx_assignments_recipe_id ON assignments (recipe_id);
-CREATE INDEX idx_assignments_episode_id ON assignments (episode_id);
-
