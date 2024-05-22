7803845b761d205fee2dc650f264074fd65a1991
liapkin <146948686+ntua-el13656@users.noreply.github.com>
2024-05-22 19:28:39 +0300

    Update 10.national_cuisines_same_participation_in_consecutive_years.sql

--- queries/10.national_cuisines_same_participation_in_consecutive_years.sql
+++ queries/10.national_cuisines_same_participation_in_consecutive_years.sql
@@ -2,7 +2,7 @@ WITH CuisineParticipation AS (
     SELECT 
         nc.id AS national_cuisine_id,
         nc.name AS national_cuisine_name,
-        YEAR(e.updated_at) AS year,
+        e.season,
         COUNT(DISTINCT e.id) AS participation_count
     FROM 
         national_cuisines nc
@@ -11,7 +11,7 @@ WITH CuisineParticipation AS (
     JOIN 
         episodes e ON s.chef_id = e.id
     GROUP BY 
-        nc.id, nc.name, YEAR(e.updated_at)
+        nc.id, nc.name, e.season
     HAVING 
         COUNT(DISTINCT e.id) >= 3
 ),
@@ -19,25 +19,25 @@ CuisineParticipationPairs AS (
     SELECT 
         cp1.national_cuisine_id AS cuisine1_id,
         cp1.national_cuisine_name AS cuisine1_name,
-        cp1.year AS year1,
+        cp1.season AS season1,
         cp1.participation_count,
         cp2.national_cuisine_id AS cuisine2_id,
         cp2.national_cuisine_name AS cuisine2_name,
-        cp2.year AS year2
+        cp2.season AS season2
     FROM 
         CuisineParticipation cp1
     JOIN 
         CuisineParticipation cp2 ON cp1.national_cuisine_id = cp2.national_cuisine_id
         AND cp1.participation_count = cp2.participation_count
-        AND cp1.year = cp2.year - 1
+        AND cp1.season = cp2.season - 1
 )
 
 SELECT 
     cuisine1_name AS national_cuisine,
-    year1,
-    year2,
+    season1,
+    season2,
     participation_count
 FROM 
     CuisineParticipationPairs
 ORDER BY 
-    cuisine1_name, year1;
+    cuisine1_name, season1;
