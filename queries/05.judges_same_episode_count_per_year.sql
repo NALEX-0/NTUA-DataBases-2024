f9593eb333b639c1b845a6ee223a048355bbcc0c
liapkin <146948686+ntua-el13656@users.noreply.github.com>
2024-05-22 19:38:54 +0300

    Update 05.judges_same_episode_count_per_year.sql

--- queries/05.judges_same_episode_count_per_year.sql
+++ queries/05.judges_same_episode_count_per_year.sql
@@ -1,11 +1,9 @@
--- Without the CLAUSE it works. With it doesn't. 
-
 WITH JudgeParticipation AS (
     SELECT 
         j.chef_id AS judge_id,
         c.first_name,
         c.last_name,
-        YEAR(e.updated_at) AS year,
+        e.season,
         COUNT(j.episode_id) AS episode_count
     FROM 
         judges j
@@ -14,51 +12,38 @@ WITH JudgeParticipation AS (
     JOIN 
         episodes e ON j.episode_id = e.id
     GROUP BY 
-        j.chef_id, c.first_name, c.last_name, YEAR(e.updated_at)
-)
-
-SELECT 
-    jp.*
-FROM 
-    JudgeParticipation jp
-ORDER BY 
-    jp.year, jp.episode_count DESC;
-
-
-
-
-
-
-WITH JudgeParticipation AS (
-    SELECT 
-        j.chef_id AS judge_id,
-        c.first_name,
-        c.last_name,
-        YEAR(e.updated_at) AS year,
-        COUNT(j.episode_id) AS episode_count
-    FROM 
-        judges j
-    JOIN 
-        chefs c ON j.chef_id = c.id
-    JOIN 
-        episodes e ON j.episode_id = e.id
-    GROUP BY 
-        j.chef_id, c.first_name, c.last_name, YEAR(e.updated_at)
+        j.chef_id, c.first_name, c.last_name, e.season
     HAVING 
-        COUNT(j.episode_id) > 3
+        COUNT(j.episode_id) > 1
+),
+SameParticipation AS (
+    SELECT 
+        jp1.judge_id AS judge1_id,
+        jp1.first_name AS judge1_first_name,
+        jp1.last_name AS judge1_last_name,
+        jp1.season,
+        jp1.episode_count,
+        jp2.judge_id AS judge2_id,
+        jp2.first_name AS judge2_first_name,
+        jp2.last_name AS judge2_last_name
+    FROM 
+        JudgeParticipation jp1
+    JOIN 
+        JudgeParticipation jp2 ON jp1.season = jp2.season
+        AND jp1.episode_count = jp2.episode_count
+        AND jp1.judge_id < jp2.judge_id 
 )
 
 SELECT 
-    jp1.judge_id,
-    jp1.first_name,
-    jp1.last_name,
-    jp1.year,
-    jp1.episode_count
+    sp.judge1_id,
+    sp.judge1_first_name,
+    sp.judge1_last_name,
+    sp.judge2_id,
+    sp.judge2_first_name,
+    sp.judge2_last_name,
+    sp.season,
+    sp.episode_count
 FROM 
-    JudgeParticipation jp1
-JOIN 
-    JudgeParticipation jp2 ON jp1.episode_count = jp2.episode_count
-    AND jp1.year = jp2.year
-    AND jp1.judge_id <> jp2.judge_id
+    SameParticipation sp
 ORDER BY 
-    jp1.year, jp1.episode_count DESC, jp1.last_name, jp1.first_name;
+    sp.season, sp.episode_count DESC, sp.judge1_last_name, sp.judge1_first_name;
