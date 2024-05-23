ddda1bb45dbecb6931450f0ca30e3c1c1936bdb4
apolitical_bliss <99957245+Alexandropoulos@users.noreply.github.com>
2024-05-23 21:21:44 +0300

    Update 02.chefs_by_cuisine_and_season.sql
    Το ερώτημα είναι ελαφρώς ασαφές. Επιλέχθηκαν οι μάγειρες που ειδικεύονται στην ελληνική εθνική κουζίνα και εμφανίζονται στη δεύτερη σεζόν του διαγωνισμού, καθώς και τα επεισόδια της σεζόν στα οποία συμμετείχαν.

--- queries/02.chefs_by_cuisine_and_season.sql
+++ queries/02.chefs_by_cuisine_and_season.sql
@@ -1,12 +1,10 @@
 SELECT 
-    c.id AS chef_id,
-    c.first_name,
-    c.last_name,
-    nc.name AS national_cuisine_name,
-    e.season,
-    e.episode
+    c.Name as "Chef",
+    nc.name AS "National Cuisine",
+    e.season AS Season,
+    e.episode AS Episode
 FROM 
-    chefs c
+    chefs_view c
 JOIN 
     specializes s ON c.id = s.chef_id
 JOIN 
@@ -16,9 +14,9 @@ JOIN
 JOIN 
     episodes e ON r.episode_id = e.id
 WHERE 
-    nc.name = 'specified_national_cuisine'  -- Replace with the actual national cuisine name
-    AND e.season = specified_season  -- Replace with the season you want
+    nc.name = "Greek"
+    AND e.season = 2
 GROUP BY 
-    c.id, c.first_name, c.last_name, nc.name, e.season, e.episode
+    c.Name, nc.name, e.season, e.episode
 ORDER BY 
-    c.last_name, c.first_name, e.season, e.episode;
+    c.Name, e.season, e.episode;
