9fe0322bae6ec1752dac4f5e18e913ee47eb1363
NikosAlexandropoulos <nikosalexa2002@gmail.com>
2024-05-24 14:29:09 +0300

    Update 03.young_chefs_with_most_recipes.sql

--- queries/03.young_chefs_with_most_recipes.sql
+++ queries/03.young_chefs_with_most_recipes.sql
@@ -1,10 +1,9 @@
 SELECT 
-    c.first_name AS "First Name",
-    c.last_name AS "Last Name",
+    c.Name AS "Chef",
     c.age AS "Age",
     COUNT(k.recipe_id) AS "Recipe Count"
 FROM 
-    chefs c
+    chefs_view c
 JOIN 
 	knows k ON k.chef_id = c.id
 WHERE 
@@ -12,5 +11,5 @@ WHERE
 GROUP BY 
     c.id
 ORDER BY 
-    COUNT(k.recipe_id) DESC, c.age, c.first_name, c.last_name
+    COUNT(k.recipe_id) DESC, c.age, c.Name
 LIMIT 15;
