25e82b045ee66f871d55c436c6c8687e2ddab403
liapkin <146948686+ntua-el13656@users.noreply.github.com>
2024-05-24 16:35:26 +0300

    Delete queries/08.indexes.sql

--- queries/08.indexes.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX IF EXISTS idx_assignments_recipe_id ON assignments;
-DROP INDEX IF EXISTS idx_assignments_episode_id ON assignments;
-
-CREATE INDEX idx_equipment_requirements_recipe_id ON equipment_requirements (recipe_id);
-CREATE INDEX idx_equipment_requirements_equipment_id ON equipment_requirements (equipment_id);
-CREATE INDEX idx_assignments_recipe_id ON assignments (recipe_id);
-CREATE INDEX idx_assignments_episode_id ON assignments (episode_id);
